ad71cee0ecc0d2c82e2f6f7babe105e0d78ce085
CristianDemeter <demeter.cri@gmail.com>
2022-11-03 17:55:03 +0100

    spostamento file

--- users/index.php
+++ users/index.php
@@ -1,18 +1,16 @@
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        Bestia proprio madonna cara
-    </body>
-    </html>
-
 <?php
 
-    echo "ciao";
+$name = $_GET['name'];
+$id = $_GET['id'];
+$email = $_GET['email'];
+$hashpassword = $_GET['hashpassword'];
+
+$id = array("a01", "b02", "c03", "d04");
+
+//a01 b02 c03 d04
+
+echo $_GET['name'] . $_GET['email'];
+
+
 ?>
