e52a20cb3667b1109d300c4539b9643b762ee235
CristianDemeter <demeter.cri@gmail.com>
2022-11-03 17:58:55 +0100

    frontend

--- users/index.php
+++ users/index.php
@@ -15,3 +15,18 @@ echo $_GET['name'] . $_GET['email'];
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restful</title>
+</head>
+<body>
+        <?php
+        echo $name . $id 
+        ?>
+</body>
+</html>
